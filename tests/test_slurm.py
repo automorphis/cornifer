9b2bc7e2d6f7d32da9d9d62b43e1a5fce5892856
automorphis <mlanetheta@gmail.com>
2023-09-27 16:26:27 -0400

    updates

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -10,6 +10,38 @@ from cornifer import NumpyRegister, ApriInfo, AposInfo
 saves_dir = Path.home() / "cornifer_slurm_testcases"
 python_command = "sage -python"
 
+def submit_batch(batch_filename):
+
+    sbatch_process = subprocess.run(["sbatch", str(batch_filename)], capture_output = True, text = True)
+    return sbatch_process.stdout[20:-1]
+
+def wait_till_running(job_id, max_sec, query_sec):
+
+    querying = True
+    start = time.time()
+
+    while querying:
+
+        if time.time() - start >= max_sec:
+            raise Exception("Ran out of time!")
+
+        time.sleep(query_sec)
+        squeue_process = subprocess.run(["squeue", "-j", job_id, "-o", "%.2t"], capture_output = True, text = True)
+        querying = "PD" in squeue_process.stdout
+
+def wait_till_not_running(job_id, max_sec, query_sec):
+
+    querying = True
+    start = time.time()
+
+    while querying:
+
+        if time.time() - start >= max_sec:
+            raise Exception("Ran out of time!")
+
+        time.sleep(query_sec)
+        squeue_process = subprocess.run(["squeue", "-j", job_id, "-o", "%.2t"], capture_output=True, text=True)
+        querying = squeue_process.stdout != "ST\n"
 
 class TestSlurm(unittest.TestCase):
 
@@ -39,17 +71,17 @@ f"""#!/usr/bin/env bash
 #SBATCH --array=1-{{1}}
 
 """)
-        allocation_query_wait_sec = 0.5
-        allocation_wait_max_sec = 60
-        running_query_wait_sec = 0.5
-        running_wait_max_sec = 15
         test_filename = saves_dir / 'test.sbatch'
         reg = NumpyRegister(saves_dir, "reg", "msg", 2 ** 40)
+        allocation_query_sec = 0.5
+        running_query_sec = 0.5
+        allocation_max_sec = 60
 
         slurm_test_main_filename = Path(__file__).parent / 'slurm_test_main1.py'
+        running_max_sec = 15
         blk_size = 100
         total_indices = 10050
-        slurm_time = running_wait_max_sec + 1
+        slurm_time = running_max_sec + 1
         apri = ApriInfo(hi = "hello")
         slurm_array_task_max = 10
 
@@ -63,33 +95,11 @@ f"""#!/usr/bin/env bash
             )
 
         print("Submitting test batch #1...")
-        sbatch_process = subprocess.run(["sbatch", str(test_filename)], capture_output = True, text = True)
-        job_id = sbatch_process.stdout[20:-1]
-        querying = True
-        start = time.time()
-
-        while querying:
-
-            if time.time() - start >= allocation_wait_max_sec:
-                raise Exception("Ran out of time!")
-
-            time.sleep(allocation_query_wait_sec)
-            squeue_process = subprocess.run(["squeue", "-j", job_id, "-o", "%.2t"], capture_output = True, text = True)
-            querying = "PD" in squeue_process.stdout
-
+        job_id = submit_batch(test_filename)
+        wait_till_running(job_id, allocation_max_sec, allocation_query_sec)
         print("Running test #1...")
-        querying = True
-        start = time.time()
-
-        while querying:
-
-            if time.time() - start >= running_wait_max_sec:
-                raise Exception("Ran out of time!")
-
-            time.sleep(running_query_wait_sec)
-            squeue_process = subprocess.run(["squeue", "-j", job_id, "-o", "%.2t"], capture_output = True, text = True)
-            querying = squeue_process.stdout != "ST\n"
-
+        wait_till_not_running(job_id, running_max_sec, running_query_sec)
+        print("Checking test #1...")
         self.assertTrue(error_filename.exists())
 
         with error_filename.open("r") as fh:
@@ -116,20 +126,25 @@ f"""#!/usr/bin/env bash
             )
 
         slurm_test_main_filename = Path(__file__).parent / 'slurm_test_main2.py'
+        running_max_sec = 600
         num_apri = 100000
+        slurm_time = running_max_sec + 1
+        apri = ApriInfo(hi = "hello")
         slurm_array_task_max = 2
-        wait_sec = 240
 
         with test_filename.open("w") as fh:
             fh.write(
-                sbatch_header.format(datetime.timedelta(seconds = wait_sec), slurm_array_task_max) +
+                sbatch_header.format(datetime.timedelta(seconds = slurm_time), slurm_array_task_max) +
                 f"srun {python_command} {slurm_test_main_filename} {saves_dir} {num_apri} "
                 f"$SLURM_ARRAY_TASK_MAX $SLURM_ARRAY_TASK_ID"
             )
 
-        subprocess.run(["sbatch", str(test_filename)])
-        print(f"waiting for {wait_sec + allocation_wait_sec} seconds....")
-        sleep(wait_sec + allocation_wait_sec)
+        print("Submitting test batch #1...")
+        job_id = submit_batch(test_filename)
+        wait_till_running(job_id, allocation_max_sec, allocation_query_sec)
+        print("Running test #1...")
+        wait_till_not_running(job_id, running_max_sec, running_query_sec)
+        print("Checking test #1...")
         self.assertTrue(error_filename.exists())
 
         with error_filename.open("r") as fh:
