6682040d676c15c12d905c89f27f684436dad783
automorphis <mlanetheta@gmail.com>
2023-09-28 15:54:23 -0400

    updates

--- tests/slurm_tests/test3.py
+++ tests/slurm_tests/test3.py
@@ -1,8 +1,9 @@
 import math
 import sys
+import time
 from pathlib import Path
 
-from cornifer import ApriInfo, load_shorthand, AposInfo
+from cornifer import ApriInfo, load_shorthand, AposInfo, DataNotFoundError
 import cornifer
 
 if __name__ == "__main__":
@@ -12,22 +13,39 @@ if __name__ == "__main__":
     slurm_array_task_max = int(sys.argv[3])
     slurm_array_task_id = int(sys.argv[4])
     reg = load_shorthand("reg", saves_dir)
+    query_sec = 0.5
+
+    if slurm_array_task_id == 1:
+
+        querying = True
+
+        with reg.open(readonly = True) as reg:
+
+            while querying:
+
+                time.sleep(query_sec)
+
+                for i in range(num_apri):
+
+                    if i % slurm_array_task_max != 0:
+
+                        try:
+                            reg.apos(AposInfo(i = i))
+
+                        except DataNotFoundError:
+                            break # i loop
+
+                else:
+                    querying = False
 
     with reg.open() as reg:
 
         for i in range(slurm_array_task_id - 1, num_apri, slurm_array_task_max):
 
-
             if slurm_array_task_id == 1 and i == 2 * slurm_array_task_max:
                 cornifer.registers._debug = 1
 
-            with (Path.home() / f"log{slurm_array_task_id}.txt").open("a") as fh:
-                fh.write(f"{cornifer.registers._debug}, {slurm_array_task_id}, {i}, {ApriInfo(i = i)}, {AposInfo(i = i + 2)}\n")
-
             reg.set_apos(ApriInfo(i = i), AposInfo(i = i + 2), exists_ok = True)
-
-            with (Path.home() / f"log{slurm_array_task_id}.txt").open("a") as fh:
-                fh.write(f"{cornifer.registers._debug}, {slurm_array_task_id}, {i}, {ApriInfo(i = i)}, {AposInfo(i = i + 2)}\n")
 
             if slurm_array_task_id == 1 and i == 2 * slurm_array_task_max:
                 cornifer.registers._debug = 0
