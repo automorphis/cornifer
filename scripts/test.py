157c48310650e06a7b3e6f99cc3712fda3db9a08
automorphis <mlanetheta@gmail.com>
2023-10-02 13:16:12 -0400

    updates

--- scripts/test.py
+++ scripts/test.py
@@ -1,11 +1,13 @@
 from multiprocessing import Pool
 from os import getpid
 
-def double(i):
-    print("I'm process", getpid())
-    return i * 2
+print("hi")
 
-if __name__ == '__main__':
-    with Pool() as pool:
-        result = pool.map(double, [1, 2, 3, 4, 5])
-        print(result)
+# def double(i):
+#     print("I'm process", getpid())
+#     return i * 2
+#
+# if __name__ == '__main__':
+#     with Pool() as pool:
+#         result = pool.map(double, [1, 2, 3, 4, 5])
+#         print(result)
