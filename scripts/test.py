ad5a0edda0ce77c00abf7c6a1a808c457ad1277b
automorphis <mlanetheta@gmail.com>
2023-10-02 15:24:10 -0400

    updates

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,12 @@
+import time
 from multiprocessing import get_context
 from os import getpid
 
 
 def f():
-    print("I'm process", getpid(), flush = True)
+    print(getpid(), time.time() % 100, flush = True)
+    time.sleep(3)
+    print(getpid(), time.time() % 100, flush = True)
 
 if __name__ == '__main__':
 
