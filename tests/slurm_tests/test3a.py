b57090431ae731874e82700c4146e3e1ad2546ae
automorphis <mlanetheta@gmail.com>
2023-10-05 15:32:13 -0400

    updates

--- tests/slurm_tests/test3a.py
+++ tests/slurm_tests/test3a.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     procs = []
 
     for i in range(num_processes):
-        procs.append(mp_ctx.Process(target = f, args = (test_home_dir, i, total_blks, num_processes)))
+        procs.append(mp_ctx.Process(target = f, args = (test_home_dir, i, total_blks, num_processes, blk_size)))
 
     for proc in procs:
         proc.start()
