bc5c68a358c1bd49d2614b50427963b3ad9b9af9
automorphis <mlanetheta@gmail.com>
2023-10-05 15:33:58 -0400

    updates

--- tests/slurm_tests/test3a.py
+++ tests/slurm_tests/test3a.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 from cornifer import ApriInfo, load_shorthand, Block
 
-def f(test_home_dir, i, total_blks, num_processes, blk_size):
+def f(test_home_dir, i, total_blks, num_processes, blk_size, total_indices):
 
     reg = load_shorthand("reg", test_home_dir)
 
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     procs = []
 
     for i in range(num_processes):
-        procs.append(mp_ctx.Process(target = f, args = (test_home_dir, i, total_blks, num_processes, blk_size)))
+        procs.append(mp_ctx.Process(target = f, args = (test_home_dir, i, total_blks, num_processes, blk_size, total_indices)))
 
     for proc in procs:
         proc.start()
