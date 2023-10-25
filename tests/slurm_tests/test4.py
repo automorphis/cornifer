a286c9ed9eede1bb086a327e237029ca72785960
automorphis <mlanetheta@gmail.com>
2023-10-25 13:12:00 -0400

    updates

--- tests/slurm_tests/test4.py
+++ tests/slurm_tests/test4.py
@@ -12,14 +12,16 @@ def f(num_procs, proc_index, reg, num_apri, num_blks, blk_len, num_active_txns, 
 
     reg._set_txn_shared_data(num_active_txns, txn_wait_event)
 
-    for i in range(proc_index, num_apri, num_procs):
+    with reg.open() as reg:
 
-        apri = ApriInfo(i = i)
-        reg.set_apos(apri, AposInfo(i = i + 1))
+        for i in range(proc_index, num_apri, num_procs):
 
-        for j in range(num_blks):
-            with Block(np.arange(j * blk_len, (j + 1) * blk_len), apri) as blk:
-                reg.append_disk_blk(blk)
+            apri = ApriInfo(i = i)
+            reg.set_apos(apri, AposInfo(i = i + 1))
+
+            for j in range(num_blks):
+                with Block(np.arange(j * blk_len, (j + 1) * blk_len), apri) as blk:
+                    reg.append_disk_blk(blk)
 
 
 if __name__ == "__main__":
