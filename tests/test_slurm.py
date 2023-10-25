730ff7b5df489af2c1878d8a16c29d34fd57ce4b
automorphis <mlanetheta@gmail.com>
2023-10-25 18:12:05 -0400

    updates. passes basic test cases.

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -479,11 +479,9 @@ class TestSlurm(unittest.TestCase):
         blk_len = 100
         timeout = 120
 
-        with (Path.home() / "parallelize.txt").open("w") as fh:
-            fh.write("")
-
         for num_procs in (1, 2, 10, 50):
-
+            with (Path.home() / "parallelize.txt").open("w") as fh:
+                fh.write("")
             write_batch_file(timeout, slurm_test_main_filename, num_procs, f'{num_apri} {num_blks} {blk_len}')
             print(f'Submitting test batch #4 (num_procs = {num_procs})...')
             self.submit_batch()
