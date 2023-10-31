810d0ab0f01b9ecfec812b535ba01da3d30ecab1
automorphis <mlanetheta@gmail.com>
2023-10-31 11:59:32 -0400

    updates.

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -488,7 +488,7 @@ class TestSlurm(unittest.TestCase):
         blk_len = 100
         timeout = 180
 
-        for num_procs in (24,):
+        for num_procs in (10,):
 
             with (Path.home() / "parallelize.txt").open("w") as fh:
                 fh.write("")
