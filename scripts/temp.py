5cea0e9143e50d389e16e055b473c0fdafa1429e
automorphis <mlanetheta@gmail.com>
2023-10-31 12:13:25 -0400

    updates.

--- scripts/temp.py
+++ scripts/temp.py
@@ -13,13 +13,21 @@ if __name__ == '__main__':
     num_procs = 10
     last_lines = {}
     file = Path.home() / 'parallelize.txt'
+    num_succeeded = 0
 
     with file.open('r') as fh:
 
         for i, line in enumerate(fh.readlines()):
+
             last_lines[get_pid(line)] = (i, line)
+
+            if 'succeeded' in line:
+                num_succeeded += 1
 
     last_lines = sorted([(key, val) for key, val in last_lines.items()], key = lambda t: t[1][0])
 
     for key, val in last_lines:
         print(key, val)
+
+    print(num_succeeded)
+    print(num_succeeded // (len(last_lines) - 1))
