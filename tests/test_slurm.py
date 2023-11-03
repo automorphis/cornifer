b2693f0766b90cfde5f3990813974e6886b6ffdd
automorphis <mlanetheta@gmail.com>
2023-11-03 16:19:29 -0400

    updates.

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -590,8 +590,9 @@ class TestSlurm(unittest.TestCase):
 
                     for j, blk in enumerate(reg.blks(apri)):
 
-                        with blk:
+                        with Block(np.arange(j * blk_len, (j + 1) * blk_len), apri) as blk_:
+
                             self.assertEqual(
                                 blk,
-                                Block(np.arange(j * blk_len, (j + 1) * blk_len), apri)
+                                blk_
                             )
