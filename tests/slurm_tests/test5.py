349346aff1fea5cb50b851583556036f263e0868
automorphis <mlanetheta@gmail.com>
2023-11-03 17:20:45 -0400

    updates.

--- tests/slurm_tests/test5.py
+++ tests/slurm_tests/test5.py
@@ -11,6 +11,7 @@ from cornifer import parallelize, NumpyRegister, ApriInfo, AposInfo, Block
 def f(num_procs, proc_index, reg, num_apri, num_blks, blk_len):
 
     file = Path.home() / 'parallelize.txt'
+    newline = '\n'
 
     with reg.open() as reg:
 
@@ -23,9 +24,9 @@ def f(num_procs, proc_index, reg, num_apri, num_blks, blk_len):
 
                 with Block(np.arange(j * blk_len, (j + 1) * blk_len), apri) as blk:
                     reg.append_disk_blk(blk)
-                with file.open('a') as fh:
-                    fh.write(
-                        f"{os.getpid()} {reg.summary()} {datetime.now().strftime('%H:%M:%S.%f')}\n")
+
+            with file.open('a') as fh:
+                fh.write(f"{os.getpid()} {i} {reg.summary().replace(newline, ' ')} {datetime.now().strftime('%H:%M:%S.%f')}\n")
 
 
 
